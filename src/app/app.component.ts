4ea4d9ddc345e2445f2cd61eede7af13335a159d
alex96brk <alexey.bakulin96@gmail.com>
2022-01-15 00:51:47 +0300

    [add]: diagramms, excel export and bug fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import * as Chart from 'chart.js';
 import { SystemCpu } from './interface/system-cpu';
 import { SystemHealth } from './interface/system-health';
 import { DashboardService } from './service/dashboard.service';
+import { ChartType } from '../app/enum/chart-type.enum';
 
 @Component({
   selector: 'app-root',
@@ -21,6 +23,9 @@ export class AppComponent implements OnInit {
   public http404Traces: any[] = [];
   public http500Traces: any[] = [];
   public httpDefaultTraces: any[] = [];
+
+  public pageSize: number = 10;
+  public page: number = 1;
 
 
   constructor(private dashBoardService: DashboardService) { }
@@ -50,10 +55,24 @@ export class AppComponent implements OnInit {
 
   }
 
+  public exportTableToExcel(): void {
+    const downloadLink = document.createElement('a');
+    const dataType = 'application/vnd.ms-excel';
+    const table = document.getElementById('httptrace-table');
+    const tableHTML = table.outerHTML.replace(/ /g, '%20');
+    const filename = 'httptrace.xls';
+    document.body.appendChild(downloadLink);
+    downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+    downloadLink.download = filename;
+    downloadLink.click();
+}
+
   private getTraces(): void {
     this.dashBoardService.getHttpTraces().subscribe(
       (response: any) => {
         this.processTraces(response.traces);
+        this.initializeBarChart();
+        this.initializePieChart();
       },
 
       (error: HttpErrorResponse) => {
@@ -114,7 +133,7 @@ export class AppComponent implements OnInit {
       (response: any) => {
         this.timestamp = Math.round(response.measurements[0].value);
         this.processUptime = this.formatUptime(this.timestamp);
-        if(isUpdateTime) {
+        if (isUpdateTime) {
           this.updateTimer();
         }
       },
@@ -133,24 +152,84 @@ export class AppComponent implements OnInit {
 
   private formatBytes(bytes: any): string {
     if (bytes === 0) {
-       return '0 Bytes';
-      }
+      return '0 Bytes';
+    }
     const k = 1024;
     const dm = 2 < 0 ? 0 : 2;
     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
     const i = Math.floor(Math.log(bytes) / Math.log(k));
     return parseFloat((bytes / Math.pow(k, i))
-                                    .toFixed(dm)) + ' ' + sizes[i];
-}
+      .toFixed(dm)) + ' ' + sizes[i];
+  }
 
-private formatUptime(timestamp: number): string {
-  const hours = Math.floor(timestamp / 60  / 60);
-  const minutes = Math.floor(timestamp /60) - (hours * 60);
-  const seconds = timestamp % 60;
-  return hours.toString().padStart(2, '0') + 'h ' +
-         minutes.toString().padStart(2, '0') + 'm ' +
-         seconds.toString().padStart(2, '0') + 's ';
-}
+  private formatUptime(timestamp: number): string {
+    const hours = Math.floor(timestamp / 60 / 60);
+    const minutes = Math.floor(timestamp / 60) - (hours * 60);
+    const seconds = timestamp % 60;
+    return hours.toString().padStart(2, '0') + 'h ' +
+      minutes.toString().padStart(2, '0') + 'm ' +
+      seconds.toString().padStart(2, '0') + 's ';
+  }
+
+  private initializeBarChart(): Chart {
+    const element = <HTMLCanvasElement>document.getElementById('barChart');
+    return new Chart(element, {
+      type: ChartType.BAR,
+      data: {
+        labels: ['200', '404', '400', '500'],
+        datasets: [{
+          data: [this.http200Traces.length, this.http404Traces.length, this.http400Traces.length, this.http500Traces.length],
+          backgroundColor: ['rgb(40,167,69)', 'rgb(0,123,255)', 'rgb(253,126,20)', 'rgb(220,53,69)'],
+          borderColor: ['rgb(40,167,69)', 'rgb(0,123,255)', 'rgb(253,126,20)', 'rgb(220,53,69)'],
+          borderWidth: 3
+        }]
+      },
+      options: {
+        title: { display: true, text: [`Last 100 Requests as of ${this.formatDate(new Date())}`] },
+        legend: { display: false },
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }
+
+  private initializePieChart(): Chart {
+    const element = <HTMLCanvasElement>document.getElementById('pieChart');
+    return new Chart(element, {
+      type: ChartType.PIE,
+      data: {
+        labels: ['200', '404', '400', '500'],
+        datasets: [{
+          data: [this.http200Traces.length, this.http404Traces.length, this.http400Traces.length, this.http500Traces.length],
+          backgroundColor: ['rgb(40,167,69)', 'rgb(0,123,255)', 'rgb(253,126,20)', 'rgb(220,53,69)'],
+          borderColor: ['rgb(40,167,69)', 'rgb(0,123,255)', 'rgb(253,126,20)', 'rgb(220,53,69)'],
+          borderWidth: 3
+        }]
+      },
+      options: {
+        title: { display: true, text: [`Last 100 Requests as of ${this.formatDate(new Date())}`] },
+        legend: { display: true },
+      }
+    });
+  }
+
+  private formatDate(date: Date): string {
+    const dd = date.getDate();
+    const mm = date.getMonth() + 1;
+    const year = date.getFullYear();
+    if (dd < 10) {
+      const day = `0${dd}`;
+    }
+    if (mm < 10) {
+      const month = `0${mm}`;
+    }
+    return `${mm}/${dd}/${year}`;
+  }
 
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +14,8 @@ import { DashboardService } from './service/dashboard.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxPaginationModule
   ],
   providers: [DashboardService],
   bootstrap: [AppComponent]
